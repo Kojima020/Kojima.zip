45921a5648b73c806b66e704f4a6f1538751a654
Uber020 <renanhs1@hotmail.com>
2025-12-01 03:23:41 -0300

    Home text update

--- kojima/src/pages/Home.js
+++ kojima/src/pages/Home.js
@@ -1,20 +1,20 @@
-import { useAsciiText, univers } from 'react-ascii-text';
+import { useAsciiText, colossal } from 'react-ascii-text';
 
 function Home() {
 
     const asciiTextRef = useAsciiText({
-        // animationCharacters: "*\"8",
+        // animationCharacters: "\"8",
         animationCharacterSpacing: 1,
         animationDelay: 2000,
-        animationDirection: 'vertical',
+        animationDirection: "vertical",
         animationInterval: 500,
         animationLoop: true,
-        animationSpeed: 45,
-        font: univers,
+        animationSpeed: 60,
+        font: colossal,
         // colossal / univers
         // https://github.com/samuelweckstrom/react-ascii-text
         // https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type+Something+&x=none&v=4&h=4&w=80&we=false
-        text: ["Kojima.zip"],
+        text: "Kojima.zip",
     });
 
 
